36a16085a48cae88b30cfdbfaece4067be4aab77
tanzim-nahid <tanzimnahid6@gmail.com>
2023-04-06 17:25:27 +0600

    read more and read

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData,useNavigation } from 'react-router-dom';
 import Loading from './Loading';
+import { useNavigate } from 'react-router-dom';
 
 const BookDetails = () => {
     const data = useLoaderData()
-    console.log(data);
+  
     const {title,subtitle,authors,year,image,publisher,desc,rating,price} = data
     const navigation=useNavigation()
     if(navigation.state==='loading'){
         return <Loading></Loading>
     }
+    const navigate = useNavigate()
+
+
+    const [description,setDescription] = useState(true)
     return (
-        <div className='flex justify-between border border-sky-500 m-16'>
+        <div className='md:flex justify-between border border-sky-500 m-16'>
             <div className='w-full border border-sky-800 m-16'>
                  <img className='w-full' src={image} alt="" />
             </div>
@@ -23,10 +28,10 @@ const BookDetails = () => {
                <p>Year:{year}</p>
                <p>Ratting:{rating}</p>
 
-               <h1 className='my-5'>{desc.substring(0, 150)}...<span className='font-bold text-blue-500 cursor-pointer'>Read More</span></h1>
+               <h1 className='my-5'>{description?desc.substring(0,80):desc}...<span onClick={()=>setDescription(!description)} className='font-bold text-blue-500 cursor-pointer'>{description?'Read More':'Read Less'}</span></h1>
 
                <div className='flex gap-6 items-center'>
-                   <button className="btn btn-info text-white px-5 ">Buy Now</button>
+                   <button onClick={()=>navigate('/')} className="btn btn-info text-white px-5 ">Buy Now</button>
                    <p className='font-bold'>{price}</p>
                </div>
             </div>
